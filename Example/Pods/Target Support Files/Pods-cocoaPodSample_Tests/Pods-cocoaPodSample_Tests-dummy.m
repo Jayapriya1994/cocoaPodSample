08626cc8103a2d60209c9025b3d22f4bc90a6ecf
Apple <apple@JP.local>
2017-12-15 13:26:12 +0530

    create bare project via pod create lib.

---
+++ Example/Pods/Target Support Files/Pods-cocoaPodSample_Tests/Pods-cocoaPodSample_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cocoaPodSample_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_cocoaPodSample_Tests
+@end
---
+++ Example/Pods/Target Support Files/cocoaPodSample/cocoaPodSample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_cocoaPodSample : NSObject
+@end
+@implementation PodsDummy_cocoaPodSample
+@end
